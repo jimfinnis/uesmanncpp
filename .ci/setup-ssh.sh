90737deaf562574a58d4a67c2f0625af7ec4236a
Jim Finnis <jim.finnis@gmail.com>
2020-03-09 22:05:30 +0000

    key fix?

--- .ci/setup-ssh.sh
+++ .ci/setup-ssh.sh
@@ -5,10 +5,11 @@ set -ev
 declare -r SSH_FILE="$(mktemp -u $HOME/.ssh/travis_temp_ssh_key_XXXX)"
 # Decrypt the file containing the private key (put the real name of the variables)
 openssl aes-256-cbc \
-  -K $encrypted_6ded536be504_key \
-  -iv $encrypted_6ded536be504_iv \
-  -in ".ci/travis_deploy_key.enc" \
-  -out "$SSH_FILE" -d
+    -K $encrypted_5944ae25418c_key \
+    -iv $encrypted_5944ae25418c_iv \
+    -in ".ci/travis_deploy_key.enc" \
+    -out "$SSH_FILE" -d
+
 # Enable SSH authentication
 chmod 600 "$SSH_FILE" \
   && printf "%s\n" \
